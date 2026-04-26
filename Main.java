b83569b94fafbe4b92b3505717cfdff03018438f
uc <an@ucui-MacBookPro.local>
2026-04-26 18:01:09 +0900

    HAN-9 [안의찬] 테스트 3

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("안의찬 지라 테스트 - Branch name: HAN-5-안의찬-지라테스트");
         System.out.println("안의찬 지라 테스트2 - Branch name: HAN-5-안의찬-지라테스트2");
+        System.out.println("안의찬 지라 테스트4 - Branch name: HAN-5-안의찬-지라테스트4");
     }
 }
