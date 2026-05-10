710ac1614f80eb6bb408b4782cf331fb823648fd
jhj2075 <flg989@naver.com>
2026-05-10 22:29:47 +0900

    [HF05-6] husky test3

--- Main.java
+++ Main.java
@@ -16,5 +16,6 @@ public class Main {
         System.out.println("git flow test-2");
         System.out.println("전형준 husky 테스트");
         System.out.println("전형준 husky 테스트2");
+        System.out.println("전형준 husky 테스트3");
     }
 }
